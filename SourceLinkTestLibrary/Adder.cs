a40ee5b6a17dc51a4ffefc7ff9bb419de2f0acec
Reilly Wood <reilly.wood@canalyst.com>
2020-06-10 09:48:04 -0700

    Initial project creation

---
+++ SourceLinkTestLibrary/Adder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SourceLinkTestLibrary
+{
+    static public class Adder
+    {
+        static public int Add(int x, int y)
+        {
+            Console.WriteLine($"I'm about to add {x} to {y}!");
+            return x + y;
+        }
+    }
+}
